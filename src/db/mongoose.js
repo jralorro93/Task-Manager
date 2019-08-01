91fa0080b53499f2a34afa6381302a0ca5ad83de
github username <github email address>
2019-08-01 00:47:10 -0400

    created db

---
+++ src/db/mongoose.js
@@ -0,0 +1,81 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+    useNewUrlParser: true, 
+    useCreateIndex: true
+})
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true, 
+        minlength: 6,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error("Password cannot contain 'password'")
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 1,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be positive number')
+            }
+        }
+    }
+})
+
+// const me = new User ({
+//     name: '    Juan    ', 
+//     email: 'juan@test.com',
+//     password: 123456,
+//     age: 25
+// })
+
+// me.save().then(() => {
+//         console.log(me)
+//     }).catch((error) => {
+//         console.log('Error!', error)
+//     })
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const task = new Task({
+  completed: false  
+})
+
+task.save().then(() => {
+    console.log(task)
+}).catch((error) => {
+    console.log('Error!', error)
+})
