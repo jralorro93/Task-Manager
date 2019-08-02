02a3cad04432e3d828f754b4214cdea2e195a20a
github username <github email address>
2019-08-02 15:36:19 -0400

    testing with promises

--- Playground/promise-chaining2.js
+++ Playground/promise-chaining2.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndRemove('5d426e3b1081a52727029937').then((user) => {
-    console.log(user)
-    return Task.countDocuments( {completed: false})
-}).then((result) => {
-    console.log(result)
+// Task.findByIdAndRemove('5d426e3b1081a52727029937').then((user) => {
+//     console.log(user)
+//     return Task.countDocuments( {completed: false})
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ completed: false })
+    return count
+}
+
+deleteTaskAndCount('5d42649558f96a257e3d21b1').then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
