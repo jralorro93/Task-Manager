43a7965ec43bd9491eac8133b521581fbef597a5
github username <github email address>
2019-08-06 17:50:40 -0400

    Added update for task

--- src/index.js
+++ src/index.js
@@ -108,6 +108,28 @@ app.post('/tasks', async (req, res) => {
     }
 })
 
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(404.).send({ error: 'Invalid Update!'})
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.status(200).send(task)
+    } catch(e) {
+        res.status(500).send(e)
+    }
+})
+
 app.listen(port, () => {
     console.log('Server is on port ' + port)
 
