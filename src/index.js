6a6d3aeaee0ef3cd710b1c42509ee9f8485a6be4
github username <github email address>
2019-08-07 16:18:24 -0400

    started login

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-
 app.use(express.json())
-
 app.use(userRouter)
 app.use(taskRouter)
 
@@ -16,3 +14,17 @@ app.listen(port, () => {
     console.log('Server is on port ' + port)
 
 })
+
+// const bcrypt = require('bcryptjs')
+
+// const myFunction = async () => {
+//     const password = 'Red12345!'
+//     const hashedPassword = await bcrypt.hash(password, 8)
+
+//     console.log(password)
+//     console.log(hashedPassword)
+
+//     const isMatch = await bcrypt.compare('Red12345!', hashedPassword)
+//     console.log(isMatch)
+// }
+// myFunction()
